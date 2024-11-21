efb43336ef6dcb05932701f6b31d05b2bb9a8393
Tonymaria <114440159+Tonymaria@users.noreply.github.com>
2024-11-21 21:05:43 +0100

    first push

---
+++ src/components/Landingpage.js
@@ -0,0 +1,192 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  position: relative;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 100px;
+  margin-left: 20px;
+`;
+
+const LeftHeader = styled.div`
+  display: flex;
+  margin-right: 40px;
+`;
+
+const Linker = styled.a`
+  font-family: "Geist Mono", monospace;
+  font-optical-sizing: auto;
+  font-weight: weight;
+  font-style: none;
+  margin-right: 30px;
+  text-decoration: none;
+  color: black;
+`;
+
+const Body = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 0.7;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+const Right = styled.div`
+  background: #f5cebd;
+  flex: 0.5;
+  margin-right: 40px;
+  margin-bottom: 50px;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const RightImage = styled.img`
+  width: 100%;
+`;
+
+const Comp = styled.div`
+  font-size: 20px;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Number = styled.div`
+  color: #343434;
+
+  span {
+    font-weight: bold;
+    font-size: 50px;
+    color: black;
+  }
+`;
+
+const Line = styled.div`
+  height: 1px;
+  width: 200px;
+  background: black;
+  margin-bottom: 13px;
+  margin-left: 10px;
+`;
+
+const Header1 = styled.div`
+  font-family: "Funnel Display", sans-serif;
+  font-weight: 700;
+  font-size: 60px;
+  width: fit-content;
+  margin-top: 20px;
+  margin-right: 50px;
+  p {
+    margin: 0;
+    width: fit-content;
+    padding: 5px 30px;
+    border: 1px solid #00a85a;
+    background-color: rgba(0, 168, 90, 0.1);
+  }
+`;
+
+const Sub = styled.div`
+  max-width: 540px;
+  font-size: 18px;
+  margin-top: 16px;
+`;
+
+const Wrapper2 = styled.div``;
+
+const Buttons = styled.div`
+  display: flex;
+  margin-top: 40px;
+`;
+
+const Button1 = styled.div`
+  background: #f58634;
+  width: 160px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  color: white;
+  cursor: pointer;
+  border-radius: 4px;
+`;
+
+const Button2 = styled.div`
+  margin-left: 20px;
+  color: #f58634;
+  border: 1px solid #f58634;
+  width: 160px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  cursor: pointer;
+  border-radius: 4px;
+`;
+
+// const Main = styled.div`
+//
+// `;
+
+const Landingpage = () => {
+  return (
+    <Main>
+      <Header>
+        <Logo src="/assets/asset1.png" />
+        <LeftHeader>
+          <Linker href="https://google.com">Facebook</Linker>
+          <Linker href="https://google.com">Linkedin</Linker>
+          <Linker href="https://google.com">Instagram</Linker>
+        </LeftHeader>
+      </Header>
+      <Body>
+        <Left>
+          <Wrapper2>
+            <Comp>
+              <Number>
+                <span>01</span>/ 02
+              </Number>
+              <Line />
+            </Comp>
+            <Header1>
+              Unlock Big Savings <br />
+              Through Group <p>Buying!</p>
+            </Header1>
+            <Sub>
+              Welcome to Fooddey, where collaboration meets savings. With our
+              cluster buying model, the more buyers that join, the lower the
+              prices go. It's simple
+            </Sub>
+            <Buttons>
+              <Button1>Register</Button1>
+              <Button2>Sponsor</Button2>
+            </Buttons>
+          </Wrapper2>
+        </Left>
+        <Right>
+          <RightImage src="/assets/asset3.jpg" />
+        </Right>
+      </Body>
+    </Main>
+  );
+};
+
+export default Landingpage;
---
+++ src/components/Mainpage.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Landingpage from "./Landingpage";
+
+const Mainpage = () => {
+  return (
+    <div>
+      <Landingpage />
+    </div>
+  );
+};
+
+export default Mainpage;
